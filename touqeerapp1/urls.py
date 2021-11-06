88cb58c8ff420acf34fb438ad505c1582b7275b9
Abubakar Zorrain <abubakarzorrain71@gmail.com>
2021-11-07 03:32:17 +0500

    Push For Heroku Files

--- touqeerapp1/urls.py
+++ touqeerapp1/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import re_path
 
 urlpatterns = [
-    path('purchases',views.purchases),
+    path('purchases',views.purchases,name="purchases"),
     path('sales/',views.sales),
     path('employee',views.employee),
     path('products',views.products),
